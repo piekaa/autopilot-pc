4fba512737d9646adc1f52800c2f1687720eaea6
Piotr <piotr.iwaniuk@piekoszek.pl>
2026-03-10 13:27:14 +0100

    Extend autopilot functionality with speed, vertical speed, and VNAV support

--- AutopilotWriter.h
+++ AutopilotWriter.h
@@ -7,12 +7,15 @@ class AutopilotWriter {
 protected:
     HANDLE* connection;
     AutopilotWriteField* heading = new AutopilotWriteField("HEADING_BUG_SET");
+    AutopilotWriteField* speed = new AutopilotWriteField("AP_SPD_VAR_SET");
     AutopilotWriteField* altitude = new AutopilotWriteField("AP_ALT_VAR_SET_ENGLISH");
+    AutopilotWriteField* verticalSpeed = new AutopilotWriteField("AP_VS_VAR_SET_ENGLISH");
     AutopilotWriteField* altitudeIndex = new AutopilotWriteField("ALTITUDE_SLOT_INDEX_SET");
     AutopilotWriteField* autoThrottleToggle = new AutopilotWriteField("AUTO_THROTTLE_ARM");
     AutopilotWriteField* altitudeHoldToggle = new AutopilotWriteField("AP_ALT_HOLD");
     AutopilotWriteField* verticalSpeedToggle = new AutopilotWriteField("AP_VS_HOLD");
     AutopilotWriteField* lNavToggle = new AutopilotWriteField("AP_NAV1_HOLD");
+    AutopilotWriteField* vNavToggle = new AutopilotWriteField("FLIGHT_LEVEL_CHANGE");
 
     std::unordered_map<std::string, unsigned long long> inputEvents;
 
@@ -26,6 +29,7 @@ public:
         SdkWriteConnection::registerField(connection, altitudeHoldToggle);
         SdkWriteConnection::registerField(connection, verticalSpeedToggle);
         SdkWriteConnection::registerField(connection, lNavToggle);
+        SdkWriteConnection::registerField(connection, vNavToggle);
         this->inputEvents = inputEvents;
 
         // for (const auto& [eventName, eventId] : inputEvents) {
@@ -49,6 +53,16 @@ public:
         SdkWriteConnection::setValue(connection, altitude);
     }
 
+    virtual void setSpeed(int speedValue) {
+        speed->value = speedValue;
+        SdkWriteConnection::setValue(connection, speed);
+    }
+
+    virtual void setVerticalSpeed(int verticalSpeedValue) {
+        verticalSpeed->value = verticalSpeedValue;
+        SdkWriteConnection::setValue(connection, verticalSpeed);
+    }
+
     virtual void toggleSpeed() {
         SdkWriteConnection::setValue(connection, autoThrottleToggle);
     }
@@ -63,6 +77,10 @@ public:
 
     virtual void toggleLNav() {
         SdkWriteConnection::setValue(connection, lNavToggle);
+    }
+
+    virtual void toggleVNav() {
+        SdkWriteConnection::setValue(connection, vNavToggle);
     }
 
     virtual ~AutopilotWriter() {
