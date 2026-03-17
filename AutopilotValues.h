58de34144de9bdb850b10824370344f58c13bcc8
Piotr <piotr.iwaniuk@piekoszek.pl>
2026-03-17 17:53:08 +0100

    Add communication protocol documentation and implement autopilot values serialization

--- AutopilotValues.h
+++ AutopilotValues.h
@@ -1,9 +1,19 @@
 #ifndef MSFS_CONTROLLER_AUTOPILOTVALUES_H
 #define MSFS_CONTROLLER_AUTOPILOTVALUES_H
 
+#include <string>
+#include <sstream>
+
 struct AutopilotValues {
   double heading;
   double altitude;
+
+  std::string toCommunicationString() {
+    std::ostringstream oss;
+    oss << "H " << static_cast<int>(heading) << "\n";
+    oss << "A " << static_cast<int>(altitude) << "\n";
+    return oss.str();
+  }
 };
 
 #endif //MSFS_CONTROLLER_AUTOPILOTVALUES_H
